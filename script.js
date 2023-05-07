b2167b13e689577fd6fd96fce43ce338fb13c026
LAPTOP-RTVPKDPS\caila <cailanee77@gmail.com>
2023-05-07 17:23:21 -0300

    adição do método finally

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-const consultaCEP = fetch(`https://viacep.com.br/ws/89000/json/`)
+const consultaCEP = fetch(`https://viacep.com.br/ws/89026789/json/`)
     .then(resposta => {
         return resposta.json()
     })
     .then(dado => {
         if(dado.erro) {
-            throw Error(404);
+            throw Error("Esse CEP não existe.");
         }
         else 
         console.log(dado);
     })
-    .catch(erro => {
-        erro = erro.toString().replace('Error: ', '');
-        switch(erro) {
-            case "404":
-                console.log('Formato certo, porém CEP inexistente.');
-            default:
-                console.log('CEP inválido.');
-        }
-    })
+    .catch(erro => console.log(erro))
+    .finally(mensagem => console.log('Processamento concluído'));
+
+console.log(consultaCEP)
