e703a6dcb043c674a8ae4678e7bf050b332ac2f1
LAPTOP-RTVPKDPS\caila <cailanee77@gmail.com>
2023-05-07 22:38:20 -0300

    Preenchendo campos de endereço automaticamente

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 async function buscaEndereco(cep) {
+    let mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
     try {
         const consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-        var consultaCEPConvertida = await consultaCEP.json();
+        const consultaCEPConvertida = await consultaCEP.json();
         if(consultaCEPConvertida.erro) {
             throw Error('CEP não existente.')
         }
+        const cidade = document.getElementById('cidade');
+        const logradouro = document.getElementById('endereco');
+        const estado = document.getElementById('estado');
+        const bairro = document.getElementById('bairro');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro;
+
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
     } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente.</p>`;
         console.log(erro);
     }
 }
